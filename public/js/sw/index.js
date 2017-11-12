b6444935d704ccdef3fffec8044a3399112a9571
AnaMaria396 <33445595+AnaMaria396@users.noreply.github.com>
2017-11-12 18:31:55 +0200

    Update index.js

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -40,3 +40,10 @@ self.addEventListener('fetch', function(event) {
 
 // TODO: listen for the "message" event, and call
 // skipWaiting if you get the appropriate message
+
+self.addEventListener('message',function(event){
+	if(event.data.action=="refresh"){
+		self.skipWaiting(); //a sw can call skipWaiting while it is waiting or installing
+		//that sw should not queue behind another service worker, it should take control
+	}
+});
